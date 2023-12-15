27424acb87209056ff19b55109a8f1bad62463d9
Berenice Osorio <45903954+ferenike@users.noreply.github.com>
2023-12-15 03:07:37 +0000

    Finaal Portfolio Website

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Berenice Osorio",
+  title: "Mechatronic Engineer",
+  email: "berenice.osorio@studentambassadors.com",
+  gitHub: "ferenike",
+  instagram: "ferenike.exe",
+  linkedIn: "ferenike",
   medium: "",
-  twitter: "microsoft",
+  twitter: "ferenike_exe",
   youTube: "Code",
 };
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mechatronic Engineering student at Benemérita Universidad Autónoma de Puebla. I love learn new things and I'm always looking for new challenges. I'm passionate about technology and I'm always looking for new ways to make it accessible to everyone.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Electronics",
+  "Control systems",
+  "Robotics",
+  "Programming",
+  "Mathematics",
+  "Mechanical design",
 ];
 
 /**
